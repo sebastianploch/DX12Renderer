3d1db6fef03f846636873a71c2e1ad2b2dbcdb0e
Sebastian <43659020+sebastianploch@users.noreply.github.com>
2023-05-29 15:45:25 +0100

    Win32 App Init

--- src/DX12Renderer.h
+++ src/DX12Renderer.h
@@ -1,8 +1,3 @@
-﻿// DX12Renderer.h : Include file for standard system include files,
-// or project specific include files.
-
 #pragma once
 
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
+#include "resource.h"

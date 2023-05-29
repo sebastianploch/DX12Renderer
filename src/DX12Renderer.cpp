dcfe6044c2220f66e227ebcef7fb9407ed44350f
Sebastian <43659020+sebastianploch@users.noreply.github.com>
2023-05-29 12:54:10 +0100

    Init CMake project

---
+++ src/DX12Renderer.cpp
@@ -0,0 +1,12 @@
+﻿// DX12Renderer.cpp : Defines the entry point for the application.
+//
+
+#include "DX12Renderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

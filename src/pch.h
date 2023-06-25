170a161a726f9c2b0912b7b945764624d425db0b
Sebastian <43659020+sebastianploch@users.noreply.github.com>
2023-06-25 17:09:55 +0100

    Added various Windows specific defines to PCH + dx/dxgi headers

--- src/pch.h
+++ src/pch.h
@@ -10,12 +10,28 @@
 #include <functional>
 
 // DX12
-#include "dxgi1_6.h"
-#include "directx/d3d12.h"
-#include "directx/d3dx12.h"
+#include <dxgi1_6.h>
+#include <directx/dxgiformat.h>
+#include <directx/d3d12.h>
+#include <directx/d3dx12.h>
+#include <dxguids/dxguids.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
 
 // Windows
 #include <SDKDDKVer.h>
+
+// Taken from https://github.com/walbourn/directx-vs-templates/blob/main/d3d12game_win32_dr/pch.h
+#define NOMINMAX
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+#define NOMCX
+#define NOSERVICE
+#define NOHELP
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
